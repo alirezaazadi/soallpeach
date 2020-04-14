98db0dc6b4a56621e8dd6cce225fb012a5b75bbd
Alireza Azadi <53966167+AlirezaAZ@users.noreply.github.com>
2020-04-14 17:29:46 +0430

    :/

--- prime/main.cpp
+++ prime/main.cpp
@@ -10,8 +10,7 @@ inline int is_prime(long int &);
 
 int main(int argc, char** argv) {
     long int num = 0L;
-    string input = argv[1];
-    ifstream fin(input, ios::in);
+    ifstream fin(argv[1], ios::in);
 
     while (!fin.eof()){
         fin >> num;
@@ -23,22 +22,29 @@ int main(int argc, char** argv) {
 
 inline int is_prime(long int & number){
     //Primality test
-    if (number <= 13){
-        if (number == 4 || number == 6 || number == 8 ||
-            number == 9 || number == 10 || number == 12)
+    if (
+            (number % 2 == 0 && (number != 2))
+            ||(number % 3 == 0 && (number != 3))
+            ||(number % 5 == 0 && (number != 5))
+            || (number % 7 == 0 && (number != 7))
+            || (number % 11 == 0 && (number != 11))
+            || (number % 13 == 0 && (number != 13))
+            ||(number % 997 == 0 && (number != 997))
+            ||(number % 857 == 0 && (number != 857))
+            ||(number % 787 == 0 && (number != 787))
+            || (number % 773 == 0 && (number != 773))
+            ||(number % 983 == 0 && (number != 983))
+            ||(number % 907 == 0 && (number != 907))
+
+            )
             return 0;
+    else {
+        auto last = static_cast<long int>(sqrt(number));
+
+        for (long int i = 5L; i <= last; i += 6) {
+            if (number % i == 0 || (number % (i + 2) == 0))
+                return 0;
+        }
         return 1;
     }
-
-    if (number % 2 == 0 || number % 3 == 0 || number % 5 == 0 ||
-        number % 7 == 0 || number % 11 == 0 || number % 13 == 0)
-        return 1;
-
-    auto last = static_cast<long int>(sqrt(number));
-
-    for (long int i = 5; i <= last; i+=6) {
-        if (number % i == 0 || (number % (i + 2) == 0))
-            return 0;
-    }
-    return 1;
 }
