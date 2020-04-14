d65ba9aedc753b14b06dbd933c1ddbe9bf619b43
Alireza Azadi <53966167+AlirezaAZ@users.noreply.github.com>
2020-04-14 15:14:28 +0430

    Using Primality test
    https://en.wikipedia.org/wiki/Primality_test

--- prime/main.cpp
+++ prime/main.cpp
@@ -22,25 +22,22 @@ int main(int argc, char** argv) {
 }
 
 inline int is_prime(long int & number){
-    if (
-            (number % 2 == 0 && (number != 2))
-            ||(number % 3 == 0 && (number != 3))
-            ||(number % 5 == 0 && (number != 5))
-            ||(number % 997 == 0 && (number != 997))
-            ||(number % 857 == 0 && (number != 857))
-            ||(number % 787 == 0 && (number != 787))
-            || (number % 773 == 0 && (number != 773))
-            ||(number % 983 == 0 && (number != 983))
-            ||(number % 907 == 0 && (number != 907))
-            || (number % 7 == 0 && (number != 7))
-            || (number % 11 == 0 && (number != 11))
-            || (number % 13 == 0 && (number != 13))
-    )
-        return 0;
-    auto last = static_cast<long int>(sqrt(number));
+    //Primality test
+    if (number <= 13){
+        if (number == 4 || number == 6 || number == 8 ||
+            number == 9 || number == 10 || number == 12)
+            return 0;
+        return 1;
+    }
 
-    for (long int i = 2; i < last; ++i) {
-        if (number % i == 0)
+    if (number % 2 == 0 || number % 3 == 0 || number % 5 == 0 ||
+        number % 7 == 0 || number % 2 == 11 || number % 13 == 0)
+        return 1;
+
+    long int last = static_cast<long int>(sqrt(number));
+
+    for (long int i = 5; i < last; i+=6) {
+        if (number % i == 0 || (number % (i + 2) == 0))
             return 0;
     }
     return 1;
